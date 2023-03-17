df911e4ee645bcce92f70c5c72b3688d128a3635
adilan waelohma <127759297+adilan547@users.noreply.github.com>
2023-03-18 03:08:04 +0700

    Loginroute
    success authen

--- dorm101/src/App.js
+++ dorm101/src/App.js
@@ -1,37 +1,18 @@
 import React from "react";
-import Oauth from "../src/components/Oauth"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/Login"
 
 
 function App() {
   return (
-    <div>
-      <section className=" bg-gray-100 min-h-screen flex items-center justify-center">
-        {/* login container  */}
-        <div className=" bg-blue-200 flex shadow-xl rounded-xl max-w-3xl">
-          {/* form */}
-          <div className=" w-1/2 ">
-            <h1 className=" font-bold text-4xl p-10 pb-1 text-blue-600">Login</h1>
-            <p className=" text-sm font-thin p-10 pt-3">
-              ยินดีต้อรับเข้าสู่ App Dormitory สำหรับการเเจ้งซ่อมเเละข้อมูลต่างๆ
-            </p>
-            <div className=" mt-10 grid grid-cols-3 items-center text-gray-400">
-              <hr className=" border-gray-400 ml-10" />
-              <p className=" text-center">Login with Google Oauth</p>
-              <hr className=" border-gray-400 mr-10" />
-            </div>
-            <Oauth />
-          </div>
-          {/* image */}
-          <div className=" w-1/2 p-5">
-            <img
-              src="https://i.pinimg.com/564x/3d/f3/7c/3df37c2a1851bb565f0b6b742a3d30c8.jpg"
-              alt="dormimg"
-              className=" rounded-xl"
-            ></img>
-          </div>
-        </div>
-      </section>
-    </div>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/dashboard" element={<Dashboard />} />
+    //     <Route path="/login" element={<Login />} />
+    //   </Routes>
+    // </BrowserRouter>
+    <div></div>
   );
 }
 
---
+++ dorm101/src/components/Login.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Oauth from "./Oauth"
+
+
+function Login() {
+  return (
+    <div>
+      <section className=" bg-gray-100 min-h-screen flex items-center justify-center">
+        {/* login container  */}
+        <div className=" bg-blue-200 flex shadow-xl rounded-xl max-w-3xl">
+          {/* form */}
+          <div className=" w-1/2 ">
+            <h1 className=" font-bold text-4xl p-10 pb-1 text-blue-600">Login</h1>
+            <p className=" text-sm font-thin p-10 pt-3">
+              ยินดีต้อรับเข้าสู่ App Dormitory สำหรับการเเจ้งซ่อมเเละข้อมูลต่างๆ
+            </p>
+            <div className=" mt-10 grid grid-cols-3 items-center text-gray-400">
+              <hr className=" border-gray-400 ml-10" />
+              <p className=" text-center">Login with Google Oauth</p>
+              <hr className=" border-gray-400 mr-10" />
+            </div>
+            <Oauth />
+          </div>
+          {/* image */}
+          <div className=" w-1/2 p-5">
+            <img
+              src="https://i.pinimg.com/564x/3d/f3/7c/3df37c2a1851bb565f0b6b742a3d30c8.jpg"
+              alt="dormimg"
+              className=" rounded-xl"
+            ></img>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Login;
